180c8b51e1a8a805e3e92ccb311460b1365377c2
Christopher McArthur <prince.chrismc@gmail.com>
2017-03-16 19:19:27 -0400

    Formatted banner with video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 (function($) {
             $(".video").background({
             source: {
-                poster: "../../images/camera.jpg",
-                mp4: "../../images/camera.mp4"
+                poster: "./images/camera.jpg",
+                mp4: "./images/camera.mp4"
             }});
         })(jQuery);
 
