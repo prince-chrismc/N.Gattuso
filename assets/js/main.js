37bfdc017c3cc7c52592b1d9b08253af2f12df57
Christopher McArthur <prince.chrismc@gmail.com>
2017-03-16 23:17:21 -0400

    Finishing touches on background slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
+/* video background */
 (function($) {
-            $(".video").background({
-            source: {
-                poster: "./images/camera.jpg",
-                mp4: "./images/camera.mp4"
-            }});
-        })(jQuery);
+    $(".video").background({
+        source: {
+            poster: "./images/camera.jpg",
+            mp4: "./images/camera.mp4"
+    }});
+})(jQuery);
 
+/* slideshow */
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .fadeIn(500)
+    .end()
+    .appendTo('#slideshow');
+}, 5000);
