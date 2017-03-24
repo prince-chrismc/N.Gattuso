ffb44d900395abf5b38a5047a4b22e7582ad2d9c
Christopher McArthur <prince.chrismc@gmail.com>
2017-03-23 20:38:22 -0400

    touched up image loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,18 @@ setInterval(function() {
     .appendTo('#slideshow');
 }, 5000);
 
-
-// imageLoad
-$('#slideshow').imagesLoaded( { background: true }, function() {
-  console.log('#container background image loaded');
-});
+// image loader
+$('#container').imagesLoaded()
+  .always( function( instance ) {
+    console.log('loading');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+  });
