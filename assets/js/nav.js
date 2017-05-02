1441f1a947cb3e290e7200d67d0c3a94ee02cbbd
chris mcarthur <prince.chrismc@gmail.com>
2017-05-02 10:33:48 -0400

    put open/close nav functions into file

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+/* very compilcated open and close for nav controls */
+
+function openNav() {
+    document.getElementById("nav").style.right = "0px";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.right = "-350px";
+}
