84163c5dd6468c0b17c3d85eafdfd8cc22196d45
Christopher McArthur <prince.chrismc@gmail.com>
2017-05-21 00:30:20 -0400

    two music boxes with only one working

--- assets/js/samples.js
+++ assets/js/samples.js
@@ -1,58 +1,65 @@
-var audio = $('#s1');
-var play = $('.fa-play');
-var pause = $('.fa-pause');
-var vup = $('.fa-volume-up');
-var vdown = $('.fa-volume-down');
-var vmute = $('.fa-volume-off');
+var audio1 = $('#s1');
+var play1 = $('.main-content').find('.fa-play').first();
+var pause1 = $('.main-content').find('.fa-pause').first();
+var vup1 = $('.main-content').find('.fa-volume-up').first();
+var vdown1 = $('.main-content').find('.fa-volume-down').first();
+var vmute1 = $('.main-content').find('.fa-volume-off').first();
 
-audio[0].volume = 0.8;
-pause.hide();
+var audio2 = $('#s2');
+var play2 = $('.main-content').find('.fa-play').last();
+var pause2 = $('.main-content').find('.fa-pause').last();
+var vup2 = $('.main-content').find('.fa-volume-up').last();
+var vdown2 = $('.main-content').find('.fa-volume-down').last();
+var vmute2 = $('.main-content').find('.fa-volume-off').last();
 
-play.click(function() {
-   audio.trigger('play');
-    pause.toggle();
-    play.toggle();
+audio1[0].volume = 0.8;
+pause1.hide();
+
+play1.click(function() {
+   audio1.trigger('play');
+    pause1.toggle();
+    play1.toggle();
 });
 
-pause.click(function() {
-   audio.trigger('pause');
-    play.toggle();
-    pause.toggle();
+pause1.click(function() {
+   audio1.trigger('pause');
+    play1.toggle();
+    pause1.toggle();
 });
 
-vup.click(volumeUp);
-vdown.click(volumeDown);
-vmute.click(toggleMuteAudio);
+vup1.click(volumeUp);
+vdown1.click(volumeDown);
+vmute1.click(toggleMuteAudio);
 
-audio[0].addEventListener('ended', function() {
-    audio[0].currentTime = 0;
-    pause.hide();
-    play.show();
+audio1[0].addEventListener('ended', function() {
+    audio1[0].currentTime = 0;
+    pause1.hide();
+    play1.show();
 });
 
 function update() {
     //console.log('timeupdate');
-    $('.progress-bar').css('width', ((audio[0].currentTime / audio[0].duration)*100) + '%').attr('aria-valuenow', (audio[0].currentTime / audio[0].duration)*100);
+    $('.main-content').find('.progress-bar').first().css('width', ((audio1[0].currentTime / audio1[0].duration)*100) + '%').attr('aria-valuenow', (audio1[0].currentTime / audio1[0].duration)*100);
 };
 
 function volumeUp(){
-    var volume = audio.prop("volume")+0.2;
+    var volume = audio1.prop("volume")+0.2;
     if(volume >= 1){
         volume = 1;
     }
-    audio.prop("volume",volume);
+    audio1.prop("volume",volume);
 }
 
 function volumeDown(){
-    var volume = audio.prop("volume")-0.2;
+    var volume = audio1.prop("volume")-0.2;
     if(volume <= 0){
         volume = 0;
     }
-    audio.prop("volume",volume);
+    audio1.prop("volume",volume);
 }
 
 function toggleMuteAudio(){
-    audio.prop("muted",!audio.prop("muted"));
+    audio1.prop("muted",!audio1.prop("muted"));
 }
 
 $(document).ready(function(){
