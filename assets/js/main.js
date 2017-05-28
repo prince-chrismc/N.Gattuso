96cdb66d93e3378d9d893e5934ad8e423de359f6
Christopher McArthur <prince.chrismc@gmail.com>
2017-05-27 22:24:39 -0400

    Made the background effect myself getting ride of formstorme since it no longer worked.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,19 @@
 /* video background */
-(function($) {
-    $(".video").background({
-        source: {
-            poster: "../images/bg-soundboard_side.jpg",
-            webm: "../images/bg-video.webm",
-            ogg: "../images/bg-video.ogv",
-            mp4: "../images/bg-video.mp4"
-    }});
-})(jQuery);
+$(document).ready(function() {
+    setTimeout(transition, 5000);
+});
 
-/* slideshow */
-$("#slideshow > div:gt(0)").hide();
+function transition()
+{
+    var slideA = $('#slideshow').find('div').last();
+    slideA.fadeOut('slow', function(){
+        slideA.insertBefore($('#slideshow').find('div').first());
+        slideA.show();
+        if(slideA.hasClass('video')) { $('#video')[0].play(); }
+    });
 
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(2000)
-    .next()
-    .fadeIn(500)
-    .end()
-    .appendTo('#slideshow');
-}, 5000);
+    setTimeout(transition, 5000);
+}
 
 // image loader
 $('#slideshow').imagesLoaded()
@@ -39,18 +33,6 @@ $('#slideshow').imagesLoaded()
         console.log('loading');
     });
 
-
-// preload
-$(document).ready(function() {
-    "use strict";
-    $('#preload').css({
-        display: 'table'
-    });
-});
-
-
 function preLoader() {
-    setTimeout(function() {
-        $('#preload').delay(1000).fadeOut(1000);
-    });
+    setTimeout(function() { $('#preload').fadeOut(1000); }, 500);
 };
