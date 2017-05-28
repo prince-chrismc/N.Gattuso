b78211aebb9576ee55072b061f7d64f82c213165
Christopher McArthur <prince.chrismc@gmail.com>
2017-05-28 14:54:11 -0400

    added code to ensure full screen

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -16,6 +16,18 @@ $(document).ready(function() {
     }
 
     oldWidth = $(window).width();
+
+    //forcing fullscreen view
+    $('body').css("height", "auto");
+    var dif = $(window).height() - $('body').height();
+    //console.log(dif);
+    var padding = $('.main-content').innerHeight() - $('.main-content').height();
+
+    if(dif+padding >= 0)
+        $('.main-content').css("padding", (dif+padding)/2+'px 0');
+    else
+        $('.main-content').css("padding", "");
+    $('body').css("height", "");
 });
 
 $(window).resize(function() {
@@ -39,6 +51,17 @@ $(window).resize(function() {
         $('#menu').html("<i class='fa fa-bars' aria-hidden='true'></i>  Menu");
     }
     oldWidth = $(window).width();
+
+
+    $('body').css("height", "auto");
+    var dif = $(window).height() - $('body').height();
+    //console.log("win: " + $(window).height() +   '   doc: ' + $(document).height() + " - body: " + $('body').height() + " = " + dif);
+    var padding = $('.main-content').innerHeight() - $('.main-content').height();
+    if(dif+padding >= 0)
+        $('.main-content').css("padding", (dif+padding)/2+'px 0');
+    else
+        $('.main-content').css("padding", "");
+    $('body').css("height", "");
 });
 
 
