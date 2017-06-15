f8d72b8218f74d543db061ef23921e425279f2ea
Christopher McArthur <prince.chrismc@gmail.com>
2017-06-14 21:38:17 -0400

    adding tooltip with time at cursor pos for seek

--- assets/js/samples.js
+++ assets/js/samples.js
@@ -6,6 +6,7 @@ var vdown1 = $('.main-content').find('.fa-volume-down').first();
 var vmute1 = $('.main-content').find('.fa-volume-off').first();
 var tminus1 = $('.main-content').find('p').first();
 var prog1 = $('#prog1');
+var tooltip1_shown = false;
 
 var audio2 = $('#s2');
 var play2 = $('.main-content').find('.fa-play').last();
@@ -15,6 +16,8 @@ var vdown2 = $('.main-content').find('.fa-volume-down').last();
 var vmute2 = $('.main-content').find('.fa-volume-off').last();
 var tminus2 = $('.main-content').find('p').last();
 var prog2 = $('#prog2');
+var tooltip2_shown = false;
+
 
 audio1[0].volume = 0.8;
 pause1.hide();
@@ -61,6 +64,35 @@ audio2[0].addEventListener('ended', function() {
 
 prog1.click(seek1);
 prog2.click(seek2);
+prog1.mousemove(function(e){
+    var offset = this.offsetWidth;
+    if(!tooltip1_shown){
+        tooltip1_shown = true;
+        $('#prog1').tooltip('show');
+        $('.tooltip').css('margin-top', '-5px');
+    }
+    $('.tooltip').css('left', e.offsetX.toString() + 'px');
+    $('.tooltip-inner').text(track1(e,offset));
+});
+prog1.mouseleave(function(){
+    $('#prog1').tooltip('hide');
+    tooltip1_shown = false;
+})
+
+prog2.mousemove(function(e){
+    var offset = this.offsetWidth;
+    if(!tooltip2_shown){
+        tooltip2_shown = true;
+        $('#prog2').tooltip('show');
+        $('.tooltip').css('margin-top', '-5px');
+    }
+    $('.tooltip').css('left', e.offsetX.toString() + 'px');
+    $('.tooltip-inner').text(track2(e,offset));
+});
+prog2.mouseleave(function(){
+    $('#prog2').tooltip('hide');
+    tooltip2_shown = false;
+})
 
 /* ----------------------------------------------------------------------- */
 /*                             UTILITY CODE !                              */
@@ -113,14 +145,19 @@ function seek1(e){
     //console.log("pb1 cliked!",e);
     var percent = e.offsetX / this.offsetWidth;
     //console.log(percent);
-    audio1[0].currentTime = percent * audio1[0].duration;
+    audio1[0].currentTime = Math.floor(percent * audio1[0].duration);
 }
 
-function seek2(e){
-    //console.log("pb1 cliked!",e);
-    var percent = e.offsetX / this.offsetWidth;
-    //console.log(percent);
-    audio2[0].currentTime = percent * audio2[0].duration;
+function track1(e,offset) {
+    var percent = e.offsetX / offset;
+    var time = percent * audio1[0].duration;
+    mins = Math.floor(time/60,10),
+    secs = Math.floor(time - mins*60);
+    var text = '' + mins.toString() + ':';
+    text += secs > 9 ? secs.toString() : '0' + secs.toString();
+    //console.log(text);
+
+    return text;
 }
 
 /* ----------------------------------------------------------------------- */
@@ -167,6 +204,33 @@ function toggleMuteAudio2(){
     audio2.prop("muted",!audio2.prop("muted"));
 }
 
+function seek2(e){
+    //console.log("pb2 cliked!",e);
+    var percent = e.offsetX / this.offsetWidth;
+    //console.log(percent);
+    audio2[0].currentTime = Math.floor(percent * audio2[0].duration);
+}
+
+function track2(e,offset) {
+    //console.log(e);
+
+    //console.log(e.offsetX);
+    //console.log(offset);
+
+    var percent = e.offsetX / offset;
+    //console.log(percent);
+    var time = percent * audio2[0].duration;
+    mins = Math.floor(time/60,10),
+    secs = Math.floor(time - mins*60);
+
+    var text = '' + mins.toString() + ':';
+    text += secs > 9 ? secs.toString() : '0' + secs.toString();
+    //console.log(text);
+
+    return text;
+}
+
+/* --------------------------------------------------------------------------------------------- */
 
 function isPlaying(audelem) { return !audelem[0].paused; }
 
