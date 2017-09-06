c81812458fd37cc2db242e58f9ea44e118243719
Christopher McArthur <prince.chrismc@gmail.com>
2017-09-06 14:42:35 -0400

    refined change language script
    primarily so I can debug from my laptop xD

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,11 +1,11 @@
 function changeLang() {
     var addr = $(location).attr('href');
 
-    if(addr.indexOf('fr') > -1){
-        addr = addr.replace('fr','en');
+    if(addr.indexOf('/fr/') > -1){
+        addr = addr.replace('/fr/','/en/');
     }
-    else if(addr.indexOf('en') > -1){
-        addr = addr.replace('en','fr');
+    else if(addr.indexOf('/en/') > -1){
+        addr = addr.replace('/en/','/fr/');
     }
 
     window.location = addr;
