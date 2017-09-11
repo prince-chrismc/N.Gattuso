691b8f04d27c4a5a8135a1173e35a3848a5246b4
Christopher McArtur <prince.chrismc@gmail.com>
2017-09-11 13:45:33 -0400

    Release 1.0.2 - Updated content and media

---
+++ assets/js/update.js
@@ -0,0 +1,22 @@
+/*
+* This script is to alert visitors with browsers
+* that are very old and might not be supported by
+* this site.
+*
+* Christopher McArthur, 07/09/2017.
+*/
+
+
+var $buoop = {vs:{i:9,f:-4,o:-4,s:8,c:-4},api:4};
+function $buo_f() {
+    var e = document.createElement("script");
+    e.src = "https://browser-update.org/update.min.js";
+    document.body.appendChild(e);
+    };
+
+try {
+    document.addEventListener("DOMContentLoaded", $buo_f,false)
+}
+catch(e) {
+    window.attachEvent("onload", $buo_f)
+}
