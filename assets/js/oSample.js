2a49fadde4a00dafe0999bd0b99041efa8a05656
Chris McArthur <prince.chrismc@gmail.com>
2018-01-03 15:43:03 -0500

    progress bar is now updated as song plays

--- assets/js/oSample.js
+++ assets/js/oSample.js
@@ -80,17 +80,6 @@ function Sample(obj, title, desc, src_audio, src_cover) {
         this.audio[0].currentTime = Math.floor(percent * this.audio[0].duration);
     }
 
-    this.update = function() {
-        //console.log('timeupdate');
-        this.prog_bar.css('width', ((this.audio[0].currentTime / this.audio[0].duration)*100) + '%').attr('aria-valuenow', (this.audio[0].currentTime / this.audio[0].duration)*100);
-
-        var rem = parseInt(this.audio[0].duration - this.audio[0].currentTime, 10),
-        mins = Math.floor(rem/60,10),
-        secs = rem - mins*60;
-
-        this.time.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
-    }
-
     this.showVolumeButton = function(id) {
         $('#range' + id).hide();
         $('#volume_btn' + id).show();
@@ -127,7 +116,7 @@ function Sample(obj, title, desc, src_audio, src_cover) {
 
     function generateHTML(obj, src_audio, src_cover) {
         obj.root.innerHTML += '<div class="col-sm-6    col-md-4"><img class="media-object" src="' + src_cover + '" alt="..." style="height: 250px; width: auto; max-width: 100%; margin: auto;"></div>';
-        obj.root.innerHTML += '<div class="col-sm-6    col-md-8"><h2 class="media-heading">' + obj.title + ' by <i>N.Gattuso</i></h2><h4>' + obj.desc + '</h4><div class="menu-controls"><i id="play_btn' + obj.id + '" class="fa fa-play fa-3x" data-toggle="tooltip" title="Play"></i><i id="pause_btn' + obj.id + '" class="fa fa-pause fa-3x" data-toggle="tooltip" title="Pause"></i><i id="volume_btn' + obj.id + '" class="fa fa-volume-down fa-3x" data-toggle="tooltip" title="Volume Adjust"></i><div id="range' + obj.id + '" class="range-slider" data-toggle="tooltip" title="Volume Adjust"><input id="slider' + obj.id + '" class="range-slider__range" type="range" value="80" min="0" max="100" step="5" for="#s2"><span class="range-slider__value">0</span></div></div><div id="prog' + obj.id + '" class="progress" data-toggle="tooltip" title="." data-trigger="manual"><div id="prog_bar' + obj.id + '" class="progress-bar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"></div><div class="progress-bar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"></div></div><div class="row"><p id="timestamp' + obj.id + '" style="float: right">0:00</p></div><audio id="audio' + obj.id + '" src="' + src_audio + '" preload="metadata"></audio></div></div>';
+        obj.root.innerHTML += '<div class="col-sm-6    col-md-8"><h2 class="media-heading">' + obj.title + ' by <i>N.Gattuso</i></h2><h4>' + obj.desc + '</h4><div class="menu-controls"><i id="play_btn' + obj.id + '" class="fa fa-play fa-3x" data-toggle="tooltip" title="Play"></i><i id="pause_btn' + obj.id + '" class="fa fa-pause fa-3x" data-toggle="tooltip" title="Pause"></i><i id="volume_btn' + obj.id + '" class="fa fa-volume-down fa-3x" data-toggle="tooltip" title="Volume Adjust"></i><div id="range' + obj.id + '" class="range-slider" data-toggle="tooltip" title="Volume Adjust"><input id="slider' + obj.id + '" class="range-slider__range" type="range" value="80" min="0" max="100" step="5" for="#s2"><span class="range-slider__value">0</span></div></div><div id="prog' + obj.id + '" class="progress" data-toggle="tooltip" title="." data-trigger="manual"><div id="prog_bar' + obj.id + '" class="progress-bar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"></div><div class="progress-bar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"></div></div><div class="row"><p id="timestamp' + obj.id + '" style="float: right">0:00</p></div><audio id="audio' + obj.id + '" src="' + src_audio + '" preload="metadata"  ontimeupdate="update(' + obj.id + ');"></audio></div></div>';
     }
     
     function track(e, offset, id) {
@@ -142,6 +131,17 @@ function Sample(obj, title, desc, src_audio, src_cover) {
 
         return text;
     }
+}
+
+function update(id) {
+    //console.log('timeupdate');
+    $('#prog_bar' + id.toString()).css('width', (($('#audio' + id.toString())[0].currentTime / $('#audio' + id.toString())[0].duration)*100) + '%').attr('aria-valuenow', ($('#audio' + id.toString())[0].currentTime / $('#audio' + id.toString())[0].duration)*100);
+
+    var rem = parseInt($('#audio' + id.toString())[0].duration - $('#audio' + id.toString())[0].currentTime, 10),
+    mins = Math.floor(rem/60,10),
+    secs = rem - mins*60;
+
+    $('#timestamp' + id.toString()).text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
 }
 
 $(document).ready(function() {
