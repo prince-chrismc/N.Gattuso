c78a4a4657c64e6231d7bbd960bf1d44ef6c1e38
Chris McArthur <prince.chrismc@gmail.com>
2018-01-03 16:34:57 -0500

    readding old range slider code
    didnt work

--- assets/js/oSample.js
+++ assets/js/oSample.js
@@ -154,3 +154,20 @@ $(document).ready(function() {
         }
     });
 });
+
+var rangeSlider = function() {
+    var slider = $('.range-slider')
+      , range = $('.range-slider__range')
+      , value = $('.range-slider__value');
+    slider.each(function() {
+        value.each(function() {
+            var value = $(this).prev().attr('value');
+            $(this).html(value);
+        });
+        range.on('input', function() {
+            $($(this).attr('for'))[0].volume = this.value / 100;
+            $(this).next(value).html(this.value);
+        });
+    });
+};
+rangeSlider();
