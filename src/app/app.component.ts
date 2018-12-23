4ac062296eb890f55b1a9f0aee71dd14bf0701e8
Chris McArthur <prince.chrismc@gmail.com>
2018-12-23 16:41:00 -0500

    now changing title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,23 @@ import {Component} from '@angular/core';
   <a routerLink="/lazy">Lazy</a>
   <a routerLink="/lazy/nested">Lazy_Nested</a>
   <router-outlet></router-outlet>
+  <br>
+  <p>
+    Select a title to set on the current HTML document:
+  </p>
+
+  <ul>
+    <li><a (click)="setTitle( 'Good morning!' )">Good morning</a>.</li>
+    <li><a (click)="setTitle( 'Good afternoon!' )">Good afternoon</a>.</li>
+    <li><a (click)="setTitle( 'Good evening!' )">Good evening</a>.</li>
+  </ul>
   `,
   styles: []
 })
 export class AppComponent {
+  public constructor(private titleService: Title ) { }
 
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
+  }
 }
